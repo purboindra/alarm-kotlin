404d1e7f6f50eb0a02da1a1e2033663d1f6e5f17
purboyndra <purboindra@gmail.com>
2024-11-24 14:12:45 +0700

    feat: url launch MainActivity

--- app/src/main/java/com/example/alarmapp/MainActivity.kt
+++ app/src/main/java/com/example/alarmapp/MainActivity.kt
@@ -1,44 +1,87 @@
 package com.example.alarmapp
 
+import android.Manifest
+import android.app.AlertDialog
+import android.content.Intent
+
+import android.content.pm.PackageManager
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.provider.Settings
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.annotation.RequiresApi
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.content.ContextCompat
 import com.example.alarmapp.ui.theme.AlarmAppTheme
 
 class MainActivity : ComponentActivity() {
+    private val requestPermissionLauncher =
+        registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted ->
+            if (isGranted) {
+                showAppSettingsDialog()
+            }
+        }
+    
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        
         enableEdgeToEdge()
+        
+        Log.d("MainActivity", "Intent received: ${intent?.action}")
+        Log.d("MainActivity", "Extra data received: ${intent?.getStringExtra("url")}")
+        
+        val extraData = intent.getStringExtra("url")
+        
         setContent {
             AlarmAppTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    ScheduleAlarmScreen(context = this, modifier = Modifier.padding(innerPadding))
+                    ScheduleAlarmScreen(
+                        context = this,
+                        modifier = Modifier.padding(innerPadding)
+                    )
                 }
             }
         }
+        
+        extraData?.let {
+            if (it.startsWith("http://") || it.startsWith("https://")) {
+                val browserIntent = Intent(Intent.ACTION_VIEW, Uri.parse(it))
+                startActivity(browserIntent)
+            }
+        }
     }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    AlarmAppTheme {
-        Greeting("Android")
+    
+    @RequiresApi(Build.VERSION_CODES.TIRAMISU)
+    override fun onStart() {
+        super.onStart()
+        if (ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.POST_NOTIFICATIONS
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            requestPermissionLauncher.launch(Manifest.permission.POST_NOTIFICATIONS)
+        }
+    }
+    
+    private fun showAppSettingsDialog() {
+        AlertDialog.Builder(this)
+            .setTitle("Permission Required")
+            .setMessage("Permission is permanently denied. Please enable it in the app settings.")
+            .setPositiveButton("Open Settings") { _, _ ->
+                val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS).apply {
+                    data = Uri.fromParts("package", packageName, null)
+                }
+                startActivity(intent)
+            }
+            .setNegativeButton("Cancel") { dialog, _ -> dialog.dismiss() }
+            .show()
     }
 }
