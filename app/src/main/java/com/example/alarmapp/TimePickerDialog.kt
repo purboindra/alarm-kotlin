809717e664cd847c4c80d04624a9912454bf2a6e
purboyndra <purboindra@gmail.com>
2024-11-24 19:39:51 +0700

    feat: TimePickerDialog

---
+++ app/src/main/java/com/example/alarmapp/TimePickerDialog.kt
@@ -0,0 +1,29 @@
+package com.example.alarmapp
+
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
+
+@Composable
+fun TimePickerDialog(
+    onDismiss: () -> Unit,
+    onConfirm: () -> Unit,
+    content: @Composable () -> Unit
+) {
+    
+    AlertDialog(
+        onDismissRequest = onDismiss,
+        dismissButton = {
+            TextButton(onClick = { onDismiss() }) {
+                Text("Dismiss")
+            }
+        },
+        confirmButton = {
+            TextButton(onClick = { onConfirm() }) {
+                Text("OK")
+            }
+        },
+        text = { content() }
+    )
+}
